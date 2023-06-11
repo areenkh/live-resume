311ec7082b234638d9001407ddcbba72ea421d89
areenkh <areen.khalaila@gmail.com>
2023-06-11 05:00:07 +0300

    made changes to multiple files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
   // replace the data below with your personal data
   personal: {
     caricature: "IMG_1116.png",
-    name: "Guilherme Borges Bastos",
-    birth: "1993-06-29",
-    email: "guilhermeborgesbastos@gmail.com",
+    name: "Areen Khalaila",
+    birth: "2001-12-27",
+    email: "areen.khalaila@gmail.com",
     phone: "+31 68 733 3020",
     location: "Gouda, South Holland, Netherlands"
   },
