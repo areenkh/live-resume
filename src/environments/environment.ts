21d05a25606f8a2e42669f7b138e20d62fa182a9
areenkh <areen.khalaila@gmail.com>
2023-06-11 15:50:18 +0300

    updated firebase info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,19 +11,42 @@ export const environment = {
     birth: "2001-12-27",
     email: "areen.khalaila@gmail.com",
     phone: "+31 68 733 3020",
-    location: "Gouda, South Holland, Netherlands"
+    location: "Waltham, Massachusetts"
   },
   // replace the dummy data below with the real firebase configs
-  firebaseConfig: {
-    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
-    authDomain: "live-resume-a575a.firebaseapp.com",
-    databaseURL: "https://live-resume-a575a.firebaseio.com",
-    projectId: "live-resume-a575a",
-    storageBucket: "live-resume-a575a.appspot.com",
-    messagingSenderId: "681076751855",
-    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
-    measurementId: "G-00VXD77WNG"
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
+  //   authDomain: "live-resume-a575a.firebaseapp.com",
+  //   databaseURL: "https://live-resume-a575a.firebaseio.com",
+  //   projectId: "live-resume-a575a",
+  //   storageBucket: "live-resume-a575a.appspot.com",
+  //   messagingSenderId: "681076751855",
+  //   appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
+  //   measurementId: "G-00VXD77WNG"
+  // }
+
+  // Import the functions you need from the SDKs you need
+  //import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+  //import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+   firebaseConfig : {
+    apiKey: "AIzaSyCheE5VCAC6o9zVGD0yvPZfU9-pcEBEDjI",
+    authDomain: "resume-website-8ecbf.firebaseapp.com",
+    projectId: "resume-website-8ecbf",
+    storageBucket: "resume-website-8ecbf.appspot.com",
+    messagingSenderId: "844507711386",
+    appId: "1:844507711386:web:e51c20c04e4a5cd1289c23",
+    measurementId: "G-Y21FNB6ZHP"
   }
+
+  // Initialize Firebase
+  //const app = initializeApp(firebaseConfig);
+  //const analytics = getAnalytics(app);
+
 };
 
 /*
