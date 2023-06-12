3aa96755a1e740cf7227d36e2985729d978a58f5
areenkh <areen.khalaila@gmail.com>
2023-06-12 19:00:23 +0300

    edit About me section

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,24 +2,32 @@ export const environment = {
   production: true,
   // replace the data below with your personal data
   personal: {
-    caricature: "gbastos-illustration@2x.png",
-    name: "Guilherme Borges Bastos",
-    birth: "1993-06-29",
-    email: "guilhermeborgesbastos@gmail.com",
-    phone: "+31 68 733 3020",
-    location: "Gouda, South Holland, Netherlands"
+    caricature: "",
+    name: "Areen Khalaila",
+    birth: "2001-12-27",
+    email: "areen.khalaila@gmail.com",
+    phone: "",
+    location: "Waltham, MA"
   },
   // replace the dummy data below with the real firebase configs
-  firebaseConfig: {
-    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
-    authDomain: "live-resume-a575a.firebaseapp.com",
-    databaseURL: "https://live-resume-a575a.firebaseio.com",
-    projectId: "live-resume-a575a",
-    storageBucket: "live-resume-a575a.appspot.com",
-    messagingSenderId: "681076751855",
-    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
-    measurementId: "G-00VXD77WNG"
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
+  //   authDomain: "live-resume-a575a.firebaseapp.com",
+  //   databaseURL: "https://live-resume-a575a.firebaseio.com",
+  //   projectId: "live-resume-a575a",
+  //   storageBucket: "live-resume-a575a.appspot.com",
+  //   messagingSenderId: "681076751855",
+  //   appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
+  //   measurementId: "G-00VXD77WNG"
+  // }
+  firebaseConfig : {
+    apiKey: "AIzaSyCheE5VCAC6o9zVGD0yvPZfU9-pcEBEDjI",
+    authDomain: "resume-website-8ecbf.firebaseapp.com",
+    projectId: "resume-website-8ecbf",
+    storageBucket: "resume-website-8ecbf.appspot.com",
+    messagingSenderId: "844507711386",
+    appId: "1:844507711386:web:e51c20c04e4a5cd1289c23",
+    measurementId: "G-Y21FNB6ZHP"
   }
-
   
 };
