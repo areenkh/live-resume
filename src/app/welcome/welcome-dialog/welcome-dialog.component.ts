0157c6a989d51d6d63b178c7a481abc20fa2d856
areenkh <areen.khalaila@gmail.com>
2023-06-30 23:09:53 +0300

    fixing welcome

--- src/app/welcome/welcome-dialog/welcome-dialog.component.ts
+++ src/app/welcome/welcome-dialog/welcome-dialog.component.ts
@@ -9,5 +9,16 @@ import { Component, OnInit, Input } from "@angular/core";
 export class WelcomeDialogComponent {
 
     constructor() {}
-
+    getData() {
+        return [
+          {
+            language: 'pt',
+            phrases: ["Meu nome é Areen.", "Seja bem-vindo(a) ao meu resumo online.", "Abaixo, você me conhecerá melhor..."]
+          },
+          {
+            language: 'en',
+            phrases: ["My name is Areen.", "Welcome to my Live Resume.", "Down below, you will know me better..."]
+          }
+        ];
+      }
 }
